39aa18c453dd9997c05fba3e73e10f9b5ce57b47
maheshmuttintidev <maheshmuttintidev@gmail.com>
2024-07-06 10:39:53 +0530

    refactor code and seo improvements

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import DOMPurify from 'dompurify';
-import Logo from '../public/logo.webp';
+import Logo from '/logo.webp';
 import { API_URL } from './lib/getConstants';
-import Helmet from 'react-helmet';
 
 const App: React.FC = () => {
   const [markdown, setMarkdown] = useState<string>('');
@@ -11,7 +10,7 @@ const App: React.FC = () => {
   const [error, setError] = useState<string>('');
 
   useEffect(() => {
-    const convertMarkdown = async () => {
+    const convertMarkdownToHtml = async () => {
       try {
         const { data } = await axios.post(`${API_URL}/convert-to-html`, {
           markdown,
@@ -25,25 +24,11 @@ const App: React.FC = () => {
         setError('');
       }
     };
-    convertMarkdown();
+    convertMarkdownToHtml();
   }, [markdown]);
 
   return (
     <>
-      <Helmet>
-        <title>Live Markdown Preview | Mahesh Muttinti</title>
-        <meta
-          name="description"
-          content="Live Markdown Preview is the fully functional Full Stack MERN Application. This will give the on live preview of markdown in html preview while you are entering."
-        />
-
-        <meta property="og:type" content="React" />
-        <meta
-          property="og:url"
-          content="https://live-markdown-previewer.maheshmuttintidev.in/"
-        />
-        <meta property="og:image" content="/favicon.ico" />
-      </Helmet>
       <div className="relative">
         <nav className="flex flex-wrap md:justify-start justify-center gap-2 items-center from-violet-200 to-violet-100 bg-gradient-to-t sticky top-0 shadow-neutral-300 shadow-md">
           <figure>
@@ -61,13 +46,21 @@ const App: React.FC = () => {
           {error ? <p className="text-red-500">{error}</p> : null}
         </section>
 
-        <main className="flex h-screen">
+        <main className="flex h-screen bg-[url('/banner_image.webp')] bg-cover bg-no-repeat bg-center">
           <section className="w-1/2 ">
             <textarea
               className="w-full h-full p-4 border-r border-gray-300 outline-none"
               value={markdown}
               onChange={(e) => {
-                setMarkdown(e.target.value);
+                const value = e.target.value;
+                const regex = /<\/?[\w\s="/.':;#-\\/\\?]+>/gi;
+                const isHtml = regex.test(value);
+                if (isHtml) {
+                  setError('Please enter valid markdown text');
+                } else {
+                  setError('');
+                  setMarkdown(e.target.value);
+                }
               }}
               placeholder="Type your Markdown here..."
             />
