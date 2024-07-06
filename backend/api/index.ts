a2a19e91a3206d3bf79222450abe9bdee3afb177
maheshmuttintidev <maheshmuttintidev@gmail.com>
2024-07-06 11:55:19 +0530

    add backend validation html input in the api

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -15,11 +15,16 @@ app.get("/", (req: Request, res: Response) => {
 
 app.post("/convert-to-html", (req: Request, res: Response) => {
   const { markdown } = req.body;
-  if (!markdown) {
-    throw new Error("Please provide a markdown");
+  if (typeof markdown !== "string") {
+    return res.json({ success: false, message: "Please enter a markdown" });
+  }
+  const regex = /<\/?[\w\s="/.':;#-\\/\\?]+>/gi;
+  const isHtml = regex.test(markdown);
+  if (isHtml) {
+    return res.json({ success: false, message: "Please enter valid markdown" });
   }
   const html = marked.parse(markdown);
-  res.json({ html });
+  return res.json({ html, success: true });
 });
 
 app.listen(port, () => {
